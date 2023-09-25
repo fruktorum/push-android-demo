37429e83f03e6777d8bb83c765f028baa41e73f2
Evgeniya Kazachanskaya <evgeniyabv@gmail.com>
2023-09-25 16:22:38 +0300

    #8692th5y2 Remove send push button disable

--- app/src/main/java/com/devinotele/exampleapp/HomeFragment.java
+++ app/src/main/java/com/devinotele/exampleapp/HomeFragment.java
@@ -107,7 +107,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 logsView.setText(logs);
                 logToCopy = logs;
                 scrollDown(logsScrollView);
-                setButtonEnable(sendPush);
             }
 
             @Override
@@ -135,26 +134,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         logsRx.unsubscribeOn(mainThread());
         onSaveInstanceState(state);
         super.onDestroy();
-    }
-
-    private void setButtonEnable(Button button) {
-        button.setEnabled(true);
-        button.setBackground(
-                ContextCompat.getDrawable(
-                        requireContext(),
-                        R.drawable.ic_button_blue
-                )
-        );
-    }
-
-    private void setButtonDisable(Button button) {
-        button.setEnabled(false);
-        button.setBackground(
-                ContextCompat.getDrawable(
-                        requireContext(),
-                        R.drawable.ic_grey_background
-                )
-        );
     }
 
     private void setUpViews() {
@@ -214,7 +193,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         }
 
         if (v.getId() == R.id.send_push) {
-            setButtonDisable(sendPush);
             try {
 
                 if (ActivityCompat.checkSelfPermission(requireContext(), POST_NOTIFICATIONS)
